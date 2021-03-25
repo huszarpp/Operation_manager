1c0dfe948d50d277d59fd3d9b10caf8bb79aa255
huszarpp <huszarpp@gmail.com>
2021-03-25 11:41:24 +0100

    Add filename_to_log class variable to Logger

--- src/logger_utils/logger.py
+++ src/logger_utils/logger.py
@@ -3,11 +3,17 @@ import datetime
 
 class Logger(object):
 
+    filename_to_log = None
+
     @staticmethod
-    def log_into_file(filename, __id, result):
+    def set_filename_to_log(filename):
+        Logger.filename_to_log = "output/" + filename
+
+    @staticmethod
+    def log_into_file(id, result):
         try:
-            f = open("output/" + filename, "a")
-            f.write("INFO   id: " + str(__id) + "  result: " + str(result)
+            f = open(Logger.filename_to_log, "a")
+            f.write("INFO   id: " + str(id) + "  result: " + str(result)
                       + "   " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
             f.close()
         except OSError:
