956578feaffa060db43064d9bda1cce7799fcaf9
huszarpp <huszarpp@gmail.com>
2021-03-25 13:41:32 +0100

    Fix datetime operations with timedelta

--- src/calculation_utils/calculation.py
+++ src/calculation_utils/calculation.py
@@ -5,7 +5,7 @@ from src.logger_utils.logger import Logger
 
 class Calculation(object):
 
-    datetime_mapper = {'sec': '%S', 'min': '%S', 'hour': '%S', 'day': '%S', 'month': '%S', 'year': '%S'}
+    datetime_mapper = {'sec': 1, 'min': 60, 'hour': 3600, 'day': 86400}
 
     def __init__(self, id, variable1, variable2, operator, variable2_type=None):
         self.id = id
@@ -42,16 +42,22 @@ class Calculation(object):
     def convert_datetime_operands(self):
         try:
             self.variable1 = datetime.datetime.strptime(self.variable1, '%Y-%m-%d %H:%M:%S')
-            self.variable2 = datetime.datetime.strptime(self.variable2, Calculation.datetime_mapper[self.variable2_type])
+            self.variable2 = int(self.variable2)
         except ValueError:
             self.is_valid_operation = False
             self.result = "No result! Cause: Illegal operand type!"
 
     def execute_operation(self):
         if self.operator == 'plus':
-            self.result = self.variable1 + self.variable2
+            self.result = self.variable1 + self.variable2 \
+                if self.variable2_type == None \
+                else self.variable1 \
+                     + datetime.timedelta(seconds=self.variable2 * Calculation.datetime_mapper[self.variable2_type])
         elif self.operator == 'minus':
-            self.result = self.variable1 - self.variable2
+            self.result = self.variable1 - self.variable2 \
+                if self.variable2_type == None \
+                else self.variable1 \
+                     - datetime.timedelta(seconds=self.variable2 * Calculation.datetime_mapper[self.variable2_type])
         elif self.operator == 'multi':
             self.result = self.variable1 * self.variable2
         elif self.operator == 'div':
@@ -65,7 +71,7 @@ class Calculation(object):
             self.is_valid_operation = False
             self.result = "No result! Cause: Illegal operator!"
 
-        if self.is_valid_operation and int(self.result) == 85:
+        if self.is_valid_operation and isinstance(self.result, float) and int(self.result) == 85:
             self.log_to_file()
 
     def log_to_file(self):
